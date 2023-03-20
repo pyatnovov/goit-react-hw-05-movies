2770a9e9485055dd33aac3fa17f010576ad87c3c
pyatnovov <109892815+pyatnovov@users.noreply.github.com>
2023-03-20 21:05:21 +0000

    Deploying to gh-pages from @ pyatnovov/goit-react-hw-05-movies@50d8ff8dc351888bf2eeb61a403ff8079c249fbc 🚀

--- static/js/911.38671287.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[911],{911:function(e,t,r){r.r(t);var s=r(87),c=r(184);t.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("ul",{children:(0,c.jsx)("li",{children:(0,c.jsx)(s.rU,{to:"cast"})})})})}}}]);
-//# sourceMappingURL=911.38671287.chunk.js.map
