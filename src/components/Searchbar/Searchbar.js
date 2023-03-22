14c46d55f9920e645efdcc51b9a5c97b9236058e
Vova2580 <109892815+pyatnovov@users.noreply.github.com>
2023-03-22 12:50:51 +0200

    Fix 2

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Form } from './Searchbar.styled';
 const Searchbar = ({ onSubmit }) => {
